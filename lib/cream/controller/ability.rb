5af0795a3c879142d3b884b7bf37b5f3d7b1a8b4
Kristian Mandrup <kmandrup@gmail.com>
2010-09-27 10:52:08 +0200

    fixed Permits Ability namespace

--- lib/cream/controller/ability.rb
+++ lib/cream/controller/ability.rb
@@ -1,7 +1,8 @@
 module Cream::Controller
   module Ability
     def current_ability
-      @current_ability ||= ::Ability.new(current_user, request)
+      @current_ability ||= Permits::Ability.new(current_user, request)
     end
   end
 end
+
