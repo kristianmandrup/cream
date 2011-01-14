f90e8966b5481949520c290e053ed5a62e55bfa4
Kristian Mandrup <kmandrup@gmail.com>
2011-01-14 14:51:29 +0100

    remember Guest user in session for #current_user

--- lib/cream/controller/application_controller.rb
+++ lib/cream/controller/application_controller.rb
@@ -1,15 +1,16 @@
 class ApplicationController < ActionController::Base
+
   def current_user
-    cu = super           
+    @cu ||= super           
     if defined? Cream::Role
       # Try to get instance of any of the available roles as the current user
       Cream::Role.available.each do |role|
         method = :"current_#{role}"
-        cu ||= send method if respond_to?(method)
+        @cu ||= send method if respond_to?(method)
       end
     end    
-    # if all else fails, the user is likely not signed in so create a Guest user for now    
-    cu ||= Guest.create if defined?(Guest)
+    return @cu if @cu
+    session[:guest] ||= Guest.create if defined?(Guest)
   end
 
   def user_signed_in?
