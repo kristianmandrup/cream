abef4802bdc24da68476f5876a17876635bfed93
Kristian Mandrup <kmandrup@gmail.com>
2011-05-20 00:26:17 +0200

    fixed some naming bugs in rails.rb

--- lib/cream/configure/rails.rb
+++ lib/cream/configure/rails.rb
@@ -6,10 +6,13 @@ require_all File.dirname(__FILE__) + '/after_init'
 Rails3::Plugin::Extender.new do
   # extend action_controller with methods from some modules
 
+  # helper modules:
+  # - ability_ext, host_ext, language_ext, permit_ext, role_attributes_ext, role_ext, session_ext, user_ext
+
   extend_rails :controller do
     extend_from_module Cream, :role
 
-    extend_from_module Cream::Helper, :role_ext,    :host_ext,    :action_ext
+    extend_from_module Cream::Helper, :role_ext,    :host_ext,    :permit_ext
     extend_from_module Cream::Helper, :ability_ext, :permit_ext,  :session_ext, :user_ext
   end  
 
@@ -17,7 +20,7 @@ Rails3::Plugin::Extender.new do
   extend_rails :view do
     extend_from_module Cream::View,   :role,        :host,        :user_action_menu
 
-    extend_from_module Cream::Helper, :role_ext,    :action_ext,  :ability_ext 
+    extend_from_module Cream::Helper, :role_ext,    :permit_ext,  :ability_ext 
     extend_from_module Cream::Helper, :permit_ext,  :session_ext, :user_ext
   end  
   
@@ -26,6 +29,7 @@ Rails3::Plugin::Extender.new do
 
     if defined? Cream::UserControl
       ApplicationController.send :include, Cream::Helper::UserExt
+      ApplicationController.send :include, Cream::Helper::SessionExt
     else
       puts "Cream::UserControl not defined!"
     end
